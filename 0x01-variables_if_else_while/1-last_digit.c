fc7ed7fd03a62f656b6982419c0b397eeb4b62f0
honnystyvn <honnystyvn@gmail.com>
2022-12-09 17:22:32 -0800

    jffh

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+/**
+ * main - initiating betty.
+ *
+ * Description: main
+ *
+ * Return : always 0 (Success)
+ */
+int main(void)
+{
+		int n;
+		int num;
+			srand(time(0));
+			n = rand() - RAND_MAX / 2;
+			printf("Last digit of %d is", n);
+			num = n % 10;
+			if (num > 5)
+				printf("%d and greater than 5", num);
+			else if (num == 0)
+				printf("%d and is 0", num);
+			else
+				printf("%d and is less than 6 and not 0", num);
+			return (0);
+
+}
