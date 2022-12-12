b2566247d32148df9009af46647058c6825873b5
honnystyvn <honnystyvn@gmail.com>
2022-12-12 08:23:44 -0800

    jfnvdjdjd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main- initiating betty.
+*
+*Description: main
+*
+*Return: always 0 (Success)
+*/
+int main()
+{
+	char c;
+	
+	c == "a";
+	while (c <= "z")
+	{
+		putchar (c);
+		c++;
+	}
+	return (0);
+}
