ed0bac1db7221c9d74dfd38ff58c798b1a7fbf9f
honnystyvn <honnystyvn@gmail.com>
2022-12-17 04:19:12 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 /**
- * main: Entry point
- *Return: 0 (Success)
+ * main: Initiating betty style
+ *
+ * Description- print string using puts
+ *
+ * Return:always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-		return (0);
+	puts("Programming is like building a multilingual puzzle");
+	puts("\n");
+	return (0);
 }
