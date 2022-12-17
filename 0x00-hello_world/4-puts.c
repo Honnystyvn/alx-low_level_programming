75941345c58f1e5cba6bbffc30a3d0127ed2f5bf
honnystyvn <honnystyvn@gmail.com>
2022-12-17 04:29:35 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main: Initiating betty style
+ * main: This function prints a string.
  *
- * Description- print string using puts
+ * Desc: print string using puts.
  *
- * Return:always 0 (Success)
+ * Return:always 0 (Success).
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	puts("\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
