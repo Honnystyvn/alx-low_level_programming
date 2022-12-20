f201b9ee9cdbf4546a42a6de3ec231c4ffee1cc3
honnystyvn <honnystyvn@gmail.com>
2022-12-19 16:42:44 -0800

    njvkv

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,6 @@
  * @src: source.
  * Return: the pointer to dest.
  */
-
 char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
@@ -19,8 +18,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		*(dest + count) = *(src + count);
 		if (*(src + count) == '\0')
-
-			break
+			break;
 		count++;
 	}
 	return (dest);
